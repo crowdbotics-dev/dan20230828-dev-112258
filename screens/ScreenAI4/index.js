768644859c483970fc550c21cb899219d2dc4375
Daniel Palacios <daniel80380@gmail.com>
2023-08-29 18:37:37 -0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/ScreenAI4/index.js
+++ screens/ScreenAI4/index.js
@@ -1,19 +1,17 @@
 import React from "react";
 import { View, Text, Image, StyleSheet, SafeAreaView, Button } from "react-native";
 
-const BlueScreen = () => {
+const RedScreen = () => {
   return <SafeAreaView style={styles.container}>
       <View style={styles.header}>
-        <Text style={styles.title}>Blue Screen</Text>
+        <Text style={styles.title}>Red Screen</Text>
       </View>
       <View style={styles.content}>
         <Image style={styles.image} source={{
         uri: "https://tinyurl.com/42evm3m3"
       }} />
         <Text style={styles.text}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
-          euismod libero, vel bibendum sapien. Nulla facilisi. Sed euismod
-          euismod libero, vel bibendum sapien. Nulla facilisi.
+          Welcome to the Red Screen! This is a sample screen for a React Native app. To get started, edit this screen's code in your text editor of choice, save the file, and then refresh the app to see your changes. Happy coding!
         </Text>
         <Button title="Click me!" onPress={() => alert("Button clicked!")} />
       </View>
@@ -23,7 +21,7 @@ const BlueScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#0077be"
+    backgroundColor: "#FF0000"
   },
   header: {
     height: 50,
@@ -52,4 +50,4 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20
   }
 });
-export default BlueScreen;
+export default RedScreen;
