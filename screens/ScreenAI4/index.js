fa0d9a55b84713ddb5913ddfb5bdae91901443b2
Daniel Palacios <daniel80380@gmail.com>
2023-08-29 18:35:40 -0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

---
+++ screens/ScreenAI4/index.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet, SafeAreaView } from 'react-native';
+
+const BlueScreen = () => {
+  return <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.title}>Blue Screen</Text>
+      </View>
+      <View style={styles.content}>
+        <Image style={styles.image} source={{
+        uri: 'https://tinyurl.com/42evm3m3'
+      }} />
+        <Text style={styles.text}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
+          euismod libero, vel bibendum sapien. Nulla facilisi. Sed euismod
+          euismod libero, vel bibendum sapien. Nulla facilisi.
+        </Text>
+      </View>
+    </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0077be'
+  },
+  header: {
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#fff'
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  image: {
+    width: 200,
+    height: 200,
+    marginBottom: 20
+  },
+  text: {
+    fontSize: 16,
+    color: '#fff',
+    textAlign: 'center',
+    paddingHorizontal: 20
+  }
+});
+export default BlueScreen;
